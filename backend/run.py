17f9ef8bba877e7ba0d35f8a64866004d8196283
Bapt252 <baptiste.coma@gmail.com>
2025-05-12 11:14:58 +0200

    Modification du port par défaut pour éviter les conflits

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,7 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    # Utiliser le port 7000 au lieu de 8000 ou 5000
+    port = int(os.environ.get('PORT', 7000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
