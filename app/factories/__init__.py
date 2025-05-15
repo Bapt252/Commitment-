76d63aa7dae29071504157971a7f936c5e451f1e
Bapt252 <baptiste.coma@gmail.com>
2025-05-15 15:00:13 +0200

    Add __init__.py to factories package

---
+++ app/factories/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Package factories pour la création des services."""
+
+# Exposer la factory pour faciliter l'importation
+from app.factories.service_factory import ServiceFactory
