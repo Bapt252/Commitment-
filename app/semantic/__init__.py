6a6e180a541c68cd61edbba82aca51388c06720f
Bapt252 <baptiste.coma@gmail.com>
2025-05-15 16:50:46 +0200

    Ajout du module semantic d'analyse sémantique des compétences

---
+++ app/semantic/__init__.py
@@ -0,0 +1,8 @@
+"""
+Module d'analyse sémantique pour Nexten SmartMatch.
+Fournit des outils avancés pour comparer les compétences entre candidats et offres d'emploi.
+"""
+
+from .analyzer import SemanticAnalyzer
+
+__all__ = ['SemanticAnalyzer']
