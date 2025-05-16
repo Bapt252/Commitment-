2e773e6a32a17d73233665c068f8e47166303526
Bapt252 <baptiste.coma@gmail.com>
2025-05-16 10:13:57 +0200

    Ajout du fichier __init__.py au répertoire app/api

--- matching-service/app/api/__init__.py
+++ matching-service/app/api/__init__.py
@@ -1,14 +1,1 @@
-from flask import Blueprint
-
-# Création du blueprint principal
-blueprint = Blueprint('api', __name__)
-
-# Import des routes
-from app.api.routes import matches, algorithms
-
-# Enregistrer les vues de l'API
-from app.api.routes.matches import matches_bp
-from app.api.routes.algorithms import algorithms_bp
-
-blueprint.register_blueprint(matches_bp)
-blueprint.register_blueprint(algorithms_bp)
+# Ce fichier permet de faire du répertoire api un package Python
