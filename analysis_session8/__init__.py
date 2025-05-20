9c0080acc30f20f3427b22bbb5f2584ad79be114
Bapt252 <baptiste.coma@gmail.com>
2025-05-20 18:14:59 +0200

    Ajout du module analysis_session8

---
+++ analysis_session8/__init__.py
@@ -0,0 +1,10 @@
+"""
+Session 8: Analyse Comportementale et Profiling Utilisateur
+Module principal d'analyse comportementale
+"""
+
+from analysis_session8.analyzer import BehavioralAnalyzer
+from analysis_session8.patterns import PatternDetector
+from analysis_session8.preferences import PreferenceScorer
+
+__all__ = ['BehavioralAnalyzer', 'PatternDetector', 'PreferenceScorer']
